c2b658e00c1afa4c1606b00a1db0448db2ec28de
Ambrish Verma <ambrishdotnet006@gmail.com>
2025-11-04 10:21:09 +0530

    Add the category table.

---
+++ Controllers/Category.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyWebC_.Controllers
+{
+    public class Category : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Data/ApplicationDBContext.cs
@@ -0,0 +1,14 @@
+﻿using BulkyWebC_.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyWebC_
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
+        {
+           
+        }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
